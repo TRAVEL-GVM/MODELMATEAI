a3079301f13b3b4e2be62d8204fec3a66a468fce
TRAVEL <ricardo.oliveira.marques@novobanco.pt>
2024-12-04 19:07:53 +0000

    Update config.py

--- ModelM/config.py
+++ ModelM/config.py
@@ -9,8 +9,7 @@ default_color1 = '#179297'
 default_color2 = '#BFCE28'
 
 sidebar_indicators = ('Raw data', 'Test 1')
-
-nmd_data_path = "https://raw.githubusercontent.com/TRAVEL-GVM/MODELMATEAI/main/ModelMateAI_test/data/mmd.xlsx"
+nmd_data_path = "https://raw.githubusercontent.com/TRAVEL-GVM/MODELMATEAI/main/ModelM/data/mmd.xlsx"
 #'https://raw.githubusercontent.com/TRAVEL-GVM/MODELMATEAI/main/ModelMateAI_test/data/mmd.xlsx'
 travel_logo_url = "https://raw.githubusercontent.com/ricardoandreom/Webscrape/refs/heads/main/travel_logo.webp"
 
