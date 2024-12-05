d2de268b239d34c4f9d4c43c37d7769cd4eeb064
TRAVEL <ricardo.oliveira.marques@novobanco.pt>
2024-12-05 14:35:50 +0000

    Update config.py

--- ModelM/config.py
+++ ModelM/config.py
@@ -14,11 +14,10 @@ nmd_data_path = "https://raw.githubusercontent.com/TRAVEL-GVM/MODELMATEAI/main/M
 travel_logo_url = "https://raw.githubusercontent.com/ricardoandreom/Webscrape/refs/heads/main/travel_logo.webp"
 
 #openai.api_key = os.getenv("OPEN_AI_KEY")
-api_key = "sk-jGBx6tpR-FMF90rhhyo600yPRQZmErQiEm9UHCAGuT3BlbkFJX0q2tKOabn70PSQTXqJ5EhFV3EhnjaB5Qg5mSQ3JEA" 
-#st.secrets["openai"]["api_key"]
+api_key = st.secrets["openai"]["api_key"]
 
 mmd_str = """
-<h6>The objective of Modelmate is to track and monitor the status of findings identified by the Detetor, assigned to the sponsor, according to the model and segment.</h6>
+<h6>The objective of Modelmate is to track and monitor the  status of findings identified by the Detetor, assigned to the sponsor, according to the model and segment.</h6>
 """
 
 
